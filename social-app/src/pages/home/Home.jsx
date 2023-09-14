108be5fa37f5538f50f8cd1da1fe27af6625515c
RuhamaMalik <malikruhama7@gmail.com>
2023-09-15 00:44:03 +0500

    set post cards

--- social-app/src/pages/home/Home.jsx
+++ social-app/src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import { useSearchParams } from "react-router-dom";
 import Header from '../../components/header/Header';
 import { NavLink } from 'react-router-dom';
 import PostCard from '../../components/cards/postcard/PostCard';
+import profileImg from '../../assets/images/profile.jpg';
+// import MenuIcon from '@mui/icons-material/Menu';
 
 const Home = () => {
 
@@ -16,6 +18,7 @@ const Home = () => {
   return (
     <>
       <Header />
+      {/* <div className={styles.homeBar}>{MenuIcon}</div> */}
       <div className={styles.layout}>
 
         <div className={styles.tabs}> {/* Side Navigation Bar / child 1*/}
@@ -33,11 +36,41 @@ const Home = () => {
           {
             currentTab === "0" ? (
 
-              <> 
-               <h1>Home</h1>
-               <PostCard />
-               <PostCard />
-               <PostCard />
+              <>
+                <div className={styles.addPost} data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                  <div className={styles.textField} >
+                    <img src={profileImg} alt="profileImg" />
+                    <div>What's on your mind?</div>
+                  </div>
+                  {/* <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  Launch demo modal
+                </button> */}
+                  {/* <div className={styles.icons}></div> */}
+                </div>
+
+                {/* Modal start */}
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                  <div class={`modal-dialog d-flex align-items-center ${styles.postModal}`}>
+                    <div class={`modal-content ${styles.h}`}>
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        ...
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Modal End */}
+
+                <PostCard />
+                <PostCard />
+                <PostCard />
               </>
             ) : currentTab === "1" ? (
               <><h1>Friends</h1></>
